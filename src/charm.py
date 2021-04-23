722b1b7e78b75c0cc578881ae33e662a3a484a2c
Jon Seager <jon@sgrs.uk>
2021-04-23 11:21:59 +0100

    Added support to change the redirect map url using juju config. Refactored to rely on config-changed method rather than gosherve-pebble-ready

--- src/charm.py
+++ src/charm.py
@@ -16,7 +16,7 @@ import logging
 
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus
 
 logger = logging.getLogger(__name__)
 
@@ -26,14 +26,37 @@ class HelloKubeconCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.gosherve_pebble_ready, self._on_gosherve_pebble_ready)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
 
-    def _on_gosherve_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API."""
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
+    def _on_config_changed(self, event):
+        """Handle the config-changed event"""
+        # Get the gosherve container so we can configure/manipulate it
+        container = self.unit.get_container("gosherve")
+        # Do not continue if the configuration is incomplete
+        if not self._check_config():
+            return
+
+        # Create a new config layer
+        layer = self._gosherve_layer()
+        # Get the current config
+        plan = container.get_plan()
+        # Check if there are any changes to services
+        if plan.services != layer["services"]:
+            # Changes were made, add the new layer
+            container.add_layer("gosherve", layer, combine=True)
+            logging.info("Added updated layer 'gosherve' to Pebble plan")
+            # Stop the service if it is already running
+            if container.get_service("gosherve").is_running():
+                container.stop("gosherve")
+            # Restart it and report a new status to Juju
+            container.start("gosherve")
+            logging.info("Restarted gosherve service")
+        # All is well, set an ActiveStatus
+        self.unit.status = ActiveStatus()
+
+    def _gosherve_layer(self):
+        """Returns a Pebble configration layer for Gosherve"""
+        return {
             "summary": "gosherve layer",
             "description": "pebble config layer for gosherve",
             "services": {
@@ -43,18 +66,19 @@ class HelloKubeconCharm(CharmBase):
                     "command": "/gosherve",
                     "startup": "enabled",
                     "environment": {
-                        "REDIRECT_MAP_URL": "https://jnsgr.uk/demo-routes"
+                        "REDIRECT_MAP_URL": self.model.config["redirect-map"]
                     },
                 }
             },
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("gosherve", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
+
+    def _check_config(self):
+        """Check that everything is in place to start Gosherve"""
+        if self.model.config["redirect-map"] == "":
+            logger.warning("Cannot start Gosherve without 'redirect-map' configuration")
+            self.unit.status = BlockedStatus("No 'redirect-map' config specified")
+            return False
+        return True
 
 
 if __name__ == "__main__":
